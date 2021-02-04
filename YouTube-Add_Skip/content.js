241a3c7ab9d79729673971746cbcdd20776f2693
komaki1996 <komaki1996@yahoo.co.jp>
2021-02-04 15:15:41 +0900

    Update content.js

--- YouTube-Add_Skip/content.js
+++ YouTube-Add_Skip/content.js
@@ -1,20 +1,16 @@
-const host_url = location.host;
+const url = location.host;
 
-if (host_url === "www.youtube.com") {
+if (url === "www.youtube.com") {
     setInterval(
         function () {
             try {
-                const elem1 = document.getElementsByClassName("ytp-ad-overlay-close-container");
-                elem1[0].click();
-            } catch (e) {
-                ;
-            }
+                const add1 = document.getElementsByClassName("ytp-ad-overlay-close-container");
+                add1[0].click();
+            } catch (e) {}
             try {
-                const elem2 = document.getElementsByClassName("ytp-ad-skip-button-container");
-                elem2[0].click();
-            } catch (e) {
-                ;
-            }
+                const add2 = document.getElementsByClassName("ytp-ad-skip-button-container");
+                add2[0].click();
+            } catch (e) {}
         }, 1000
     )
 }
